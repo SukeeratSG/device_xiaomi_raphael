ebf32eb3aa68a37f3a77271349e6652eb07e2fa9
Sukeerat Goindi <sukeeratgoindi@gmail.com>
2020-08-11 11:05:21 +0530

    Raphael : Add VowIfi Prop's

--- system_prop.mk
+++ system_prop.mk
@@ -106,3 +106,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Zygote preforking
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
+
+# VoWifi Fix
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan=1 \
+    persist.data.iwlan=1 \
+    persist.data.iwlan.ipsec.ap=1 \
+    persist.sys.cust.lte_config=true \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.volte.dan_support=true \
+    persist.rcs.supported=1 
+
