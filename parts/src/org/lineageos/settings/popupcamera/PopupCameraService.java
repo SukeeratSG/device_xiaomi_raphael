530434a237bfdcbfa27f323231943925c1cdf708
Ido Ben-Hur <idoybh2@gmail.com>
2020-08-28 23:42:38 +0530

    raphael: parts: Allow to always display front camera open dialog
    [@ poad42]
    * Add an icon to the switch to make it consistent with other settings
    * Adapt to our parts impl
    * Ditch the perf category part since this already under pop up camera settings

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -41,6 +41,7 @@ import android.os.Message;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.WindowManager;
 
@@ -55,6 +56,7 @@ public class PopupCameraService extends Service implements Handler.Callback {
 
     private static final String TAG = "PopupCameraService";
     private static final boolean DEBUG = false;
+    private static final String alwaysOnDialogKey = "always_on_camera_dialog";
 
     private int[] mSounds;
     private boolean mMotorBusy = false;
@@ -422,7 +424,9 @@ public class PopupCameraService extends Service implements Handler.Callback {
             }
             break;
             case Constants.MSG_CAMERA_OPEN: {
-            if (!mScreenOn) {
+            boolean alwaysOnDialog = Settings.System.getInt(getContentResolver(),
+                        alwaysOnDialogKey, 0) == 1;
+            if (alwaysOnDialog || !mScreenOn) {
                 if (mAlertDialog == null) {
                     mAlertDialog = new AlertDialog.Builder(this)
                             .setMessage(R.string.popup_camera_dialog_message)
