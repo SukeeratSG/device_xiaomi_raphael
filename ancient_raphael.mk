8f5f53ca35791acc7f3e34fdb6d85bf305210cd0
SukeeratSG <sukeeratgoindi@gmail.com>
2020-09-17 09:12:34 +0530

    Beautify

--- ancient_raphael.mk
+++ ancient_raphael.mk
@@ -1,10 +1,14 @@
 #
-# Copyright (C) 2019 The Project Bliss
+# Copyright (C) 2019 The Project Ancient 
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit some device specific stuff from :
 $(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Inherit some ancient stuff from :
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1440
 
@@ -14,10 +18,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := ancient_raphael
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
 IS_PHONE := true
