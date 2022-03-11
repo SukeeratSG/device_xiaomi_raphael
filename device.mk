8eca4573150da6adcae763621cadf8f2f002cc9e
sukeeratsg <sukeerat@goindi.org>
2022-03-11 10:46:35 +0530

    raphael : overlay cleanup - Remove useless overlays - Add some specific ones - Combine dual files for simplicity
    Signed-off-by: sukeeratsg <sukeerat@goindi.org>

--- device.mk
+++ device.mk
@@ -38,9 +38,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay
 
 # ANT+
 PRODUCT_PACKAGES += \
